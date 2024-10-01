b9a9a1fa0436c0a181975275aabc43031c179dbb
Tim Dobson <hello@tdobson.net>
2024-10-01 17:18:13 +0100

    feat: Add navigation and recipe template

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,9 @@ module.exports = function(eleventyConfig) {
   // Copy the recipes folder to the output
   eleventyConfig.addPassthroughCopy("recipes");
   
+  // Copy image files
+  eleventyConfig.addPassthroughCopy("images");
+  
   // Ignore the transcripts folder
   eleventyConfig.ignores.add("transcripts/**");
 
